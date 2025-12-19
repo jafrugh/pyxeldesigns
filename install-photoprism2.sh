baa4e2fe7d70f8cb8c8745ee34a2e808eb3b1327
Adam Darling <adamad@hotmail.com>
2025-12-19 17:13:10 -0600

    Update install-photoprism2.sh

--- install-photoprism2.sh
+++ install-photoprism2.sh
@@ -12,7 +12,7 @@ STORAGE="local-lvm"
 ROOT_PASSWORD="Dolphin1!"
 
 # NAS / SMB settings
-NAS_SERVER="192.168.1.14"
+NAS_SERVER="192.168.10.14"
 NAS_SHARE="Photos"
 NAS_USER="jafrugh"
 NAS_PASS="Dolphin1!"
